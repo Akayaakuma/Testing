d071461ab220f8b6c60fa41ffd313bea591ae882
Alexander <alex.p@niftynet.de>
2020-09-01 01:34:31 +0200

    Try 1

---
+++ NoctHelper/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoctHelper
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport(dllName:"user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowTextLength(IntPtr hwnd);
+
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextW([In] IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] [Out]
+            StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowThreadProcessId([In] IntPtr hWnd, ref int lpdwProcessId);
+
+        private const int LEFTUP = 0x0004;
+        private const int LEFTDOWN = 0x0002;
+
+        private string currentWindow = "";
+        private int currentPID;
+        private int mcpid;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GetForeGroundWindowInfo()
+        {
+            IntPtr foregroundWindow = GetForegroundWindow();
+                if (!foregroundWindow.Equals(IntPtr.Zero))
+            {
+                int windowTextLength = GetWindowTextLength(foregroundWindow);
+                StringBuilder stringBuilder = new StringBuilder("", windowTextLength +1);
+                if (windowTextLength > 0)
+                {
+                    GetWindowTextW(foregroundWindow, stringBuilder, stringBuilder.Capacity);
+                }
+
+                int currentpid = 0;
+                GetWindowThreadProcessId(foregroundWindow, ref currentpid);
+                Process[] processByName = Process.GetProcessesByName("javaw");
+                foreach (Process process in processByName)
+                {
+                    mcpid = process.Id;
+                }
+
+                currentWindow = stringBuilder.ToString();
+                currentPID = currentpid;
+            }
+        }
+
+
+        private void clicktimer_Tick(object sender, EventArgs e)
+        {
+            GetForeGroundWindowInfo();
+            Random rnd = new Random();
+            int maxcps = (int)Math.Round(1000.0 / (trackBar1.Value + trackBar2.Value * 0.2));
+            int mincps = (int)Math.Round(1000.0 / (trackBar1.Value + trackBar2.Value * 0.4));
+            try
+            {
+                clicktimer.Interval = rnd.Next(mincps, maxcps);
+            }
+            catch
+            {
+                //Ignored
+            }
+
+            if(currentPID == Process.GetCurrentProcess().Id)
+            {
+                return;
+            }
+
+            if (checkBox1.Checked && currentPID != mcpid && mcpid !=0)
+            {
+                return;
+            }
+
+            bool mousdown = MouseButtons == MouseButtons.Left;
+            if (mousdown)
+            {
+                mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                Thread.Sleep(millisecondsTimeout: rnd.Next(1, 6));
+                mouse_event(dwFlags: LEFTDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Contains("on"))
+            {
+                clicktimer.Start();
+                button1.Text = "Toggle: off";
+            }
+            else
+            {
+                clicktimer.Stop();
+                button1.Text = "Toggle: on";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = $"Max Cps: {trackBar1.Value}";
+        }
+
+        private void trackBar2_Scroll_1(object sender, EventArgs e)
+        {
+            label2.Text = $"Min Cps: {trackBar2.Value}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string exename = AppDomain.CurrentDomain.FriendlyName;
+            DirectoryInfo d = new DirectoryInfo(path: @"C:\Windows\Prefetch");
+            FileInfo[] Files = d.GetFiles(searchPattern: exename + "*");
+            foreach (FileInfo file in Files)
+            {
+                File.Delete(file.FullName);
+            }
+
+            Environment.Exit(0);
+
+        }
+    }
+}
---
+++ NoctHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("NoctHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoctHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("de51bf04-601d-4622-b31a-5d34611c2a2c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
